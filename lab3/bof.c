a48fbdc1e96fff6aa020cbe012d4947afbe9b4d6
Shayan Amani <7shayan7@gmail.com>
2020-05-08 21:42:31 -0400

    lab1 finalized and lab3 started

---
+++ lab3/bof.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int chkserial(char *s)
+{
+     char buffer[16];
+     strcpy(buffer, s);
+
+     return strcmp(buffer, "cs727827");
+}
+
+void fullversion()
+{
+     printf("Thanks for purchasing! Enjoy the full-version software!\n");
+}
+
+void trialversion()
+{
+     printf("This is a trial version. Please purchase the full version to enable all features!\n");
+}
+
+int main(int argc, char **argv)
+{
+     char buf[512];
+     FILE *f = fopen(argv[1], "r");
+
+     fread(buf, 512, 1, f);
+
+     printf("buf: %s\n", buf);
+
+     if (chkserial(buf) == 0)
+          fullversion();
+     else
+          trialversion();
+
+     return 0;
+}
