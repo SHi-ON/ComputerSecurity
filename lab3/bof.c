4b690aee852266a3d75ccfef5d4000e277c1071f
Shayan Amani <7shayan7@gmail.com>
2020-05-14 19:22:10 -0400

    lab 3 finished

--- lab3/bof.c
+++ lab3/bof.c
@@ -3,40 +3,37 @@
 
 int chkserial(char *s)
 {
-     char buffer[16];
+    char buffer[16];
+    strcpy(buffer, s);
 
-//     strcpy(buffer, s);
-     strncpy(buffer, s,8);
-
-     int res = strncmp(buffer, "cs727827",8);
-//     int res = strcmp(buffer, "cs727827");
-
-     return res;
+    return strcmp(buffer, "cs727827");
 }
 
 void fullversion()
 {
-     printf("Thanks for purchasing! Enjoy the full-version software!\n");
+    printf("Thanks for purchasing! Enjoy the full-version software!\n");
 }
 
 void trialversion()
 {
-     printf("This is a trial version. Please purchase the full version to enable all features!\n");
+    printf("This is a trial version. Please purchase the full version to enable all features!\n");
 }
 
 int main(int argc, char **argv)
 {
-     char buf[512];
-     FILE *f = fopen(argv[1], "r");
+    char buf[512];
+    FILE *f = fopen(argv[1], "r");
 
-     fread(buf, 512, 1, f);
+    fread(buf, 512, 1, f);
 
-     printf("buf: %s\n", buf);
+    printf("buf: %s\n", buf);
 
-     if (chkserial(buf) == 0)
-          fullversion();
-     else
-          trialversion();
+    fullversion();
 
-     return 0;
+//    if (chkserial(buf) == 0)
+//        fullversion();
+//    else
+//        trialversion();
+
+    return 0;
 }
